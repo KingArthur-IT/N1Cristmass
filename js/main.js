15303e3e0b836c84300c5334a356637ea735aa14
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-11-05 22:45:55 +0300

    mobile menu & js

--- js/main.js
+++ js/main.js
@@ -28,6 +28,17 @@ tabBtns.forEach((el, index) => {
     })
 });
 
+//MOBILE MENU
+const mobileMenu = document.querySelector('.mobile-menu');
+
+document.querySelector('.header__burger').addEventListener('click', () => {
+    mobileMenu?.classList.add('opened');
+});
+
+document.querySelector('.mobile-menu__close').addEventListener('click', () => {
+    mobileMenu?.classList.remove('opened');
+});
+
 //MODAL
 const modal = document.querySelector('.modal');
 const modalForm = document.querySelector('.modal__hero');
@@ -35,6 +46,7 @@ const modalThanks = document.querySelector('.modal__thanks');
 
 document.querySelectorAll('.show-form').forEach((el) => {
     el.addEventListener('click', () => {
+        mobileMenu?.classList.remove('opened');
         if (modal){
             modal.classList.add('displayed');
             setTimeout(() => {
@@ -61,11 +73,21 @@ document.querySelector('.modal__close')?.addEventListener('click', () => {
     document.getElementById(`${el}-partners-send`).addEventListener('click', (e) => {
         e.preventDefault();
 
+        // get values
         const companyName = document.getElementById(`${el}-company-name`)?.value;
         const phone = document.getElementById(`${el}-phone`)?.value;
         const email = document.getElementById(`${el}-email`)?.value;
         const message = document.getElementById(`${el}-message`)?.value;
 
+        //send
+
+        //clear
+        document.getElementById(`${el}-company-name`).value = '';
+        document.getElementById(`${el}-phone`).value = '';
+        document.getElementById(`${el}-email`).value = '';
+        document.getElementById(`${el}-message`).value = '';
+
+        //show thanks
         modalForm.classList.add('d-none');
         modalThanks.classList.remove('d-none');
     })    
