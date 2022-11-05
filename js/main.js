854de1aead7f1ae458538872eee33bd1e604a69c
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-11-05 20:43:07 +0300

    form init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//HEADER
 document.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const header = document.querySelector('.header');
@@ -5,4 +6,47 @@ document.addEventListener('scroll', () => {
         header.classList.add('fixed');
     if (scrollTop < 20 && header.classList.contains('fixed')) 
         header.classList.remove('fixed');
+});
+
+//TABS
+const tabBtns = document.querySelectorAll('.modal__tab-btn');
+tabBtns.forEach((el, index) => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault();
+        //change tab active
+        el.classList.add('active');
+        const notActiveTabIndex = index === 0 ? 1 : 0;
+        tabBtns[notActiveTabIndex].classList.remove('active');
+        //change content display
+        if (index === 0){
+            document.querySelector('.modal__new-partners')?.classList.add('active');
+            document.querySelector('.modal__current-partners')?.classList.remove('active');
+        } else{
+            document.querySelector('.modal__new-partners')?.classList.remove('active');
+            document.querySelector('.modal__current-partners')?.classList.add('active');
+        }
+    })
+});
+
+//MODAL
+const modal = document.querySelector('.modal');
+
+document.querySelectorAll('.show-form').forEach((el) => {
+    el.addEventListener('click', () => {
+        if (modal){
+            modal.classList.add('displayed');
+            setTimeout(() => {
+                modal.classList.add('visible');
+            }, 300);
+        }
+    })
 })
+
+document.querySelector('.modal__close')?.addEventListener('click', () => {
+    if (modal){
+        modal.classList.remove('visible');
+        setTimeout(() => {
+            modal.classList.remove('displayed');
+        }, 300);
+    }
+});
